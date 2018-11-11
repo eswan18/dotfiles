9bfa76d2975a370fafd315e93853cc62695759d5
Ethan Swan <eswan18@rocketmail.com>
2018-11-10 20:16:36 -0500

    update_remote script working

--- update_remote.py
+++ update_remote.py
@@ -1,5 +1,8 @@
 from fabric import task, Connection
-import getpass
+import getpass, sys
+from paramiko import AuthenticationException
+
+repo_remote = 'https://github.com/eswan18/dotfiles.git'
 
 with open('serverlist.txt') as f:
     servers = [server.strip() for server in f.readlines()]
@@ -7,4 +10,8 @@ with open('serverlist.txt') as f:
 for server in servers:
     p = getpass.getpass(f'Password for {server} ')
     c = Connection(server, connect_kwargs={'password': p})
-    c.run('echo $HOSTNAME')
+    result = c.run('mktemp -d', hide='out')
+    tempdir = result.stdout.strip()
+    result = c.run(f'cd {tempdir} && git clone {repo_remote}', hide='out')
+    result = c.run(f'cd {tempdir}/dotfiles && ./update_local')
+    print()
